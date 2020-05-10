e5e7549305cce9db2495e44ca727b25d001966e8
James Smith <james.smith@openmathematics.org>
2020-05-10 12:01:52 +0100

    Exported the FiraCode mixin.

--- es6/index.js
+++ es6/index.js
@@ -1,4 +1,5 @@
 'use strict';
 
+export { default as firaCodeMixin } from "./mixin/firaCode";
 export { default as renderSyntaxStyle } from "./renderSyntaxStyle";
 export { default as renderFiraCodeStyle } from "./renderFiraCodeStyle";
