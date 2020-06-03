d1ece4360a1870576eb3d024eaa8d28173f4180a
James Smith <james.smith@openmathematics.org>
2020-06-03 17:15:59 +0100

    Propagated.

---
+++ es6/mixin/font/inherited.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const inheritedFontMixin = `
+
+  font-size: inherit;
+  line-height: inherit;
+  font-family: inherit;
+  text-rendering: inherit;
+  font-feature-settings: inherit;
+
+`;
+
+export default inheritedFontMixin;
--- es6/style/syntax.js
+++ es6/style/syntax.js
@@ -1,7 +1,7 @@
 "use strict";
 
 import plainSyntaxStyle from "../style/syntax/plain";
-import firaCodeFontMixin from "../mixin/font/firaCode";
+import inheritedFontMixin from "../mixin/font/inherited";
 import defaultSyntaxStyle from "../style/syntax/default";
 import florenceSyntaxStyle from "../style/syntax/florence";
 import metaJSONeSyntaxStyle from "../style/syntax/metaJSON";
@@ -11,7 +11,9 @@ const syntaxStyle = `
     
   .syntax,
   .syntax > * {
-    ${firaCodeFontMixin}
+  
+    ${inheritedFontMixin}
+
   }
 
   ${defaultSyntaxStyle}
