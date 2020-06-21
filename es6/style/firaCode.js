b21612a333b6ba798963177e40aa6e09b3c0c4a8
James Smith <james.smith@openmathematics.org>
2020-06-21 18:29:40 +0100

    Updated the FiraCode font.

--- es6/style/firaCode.js
+++ es6/style/firaCode.js
@@ -5,7 +5,7 @@ const firaCodeStyle = `
   @font-face {
     font-family: "Fira Code";
     src: url("css/woff2/FiraCode-Light.woff2") format("woff2"),
-    url("css/woff2/FiraCode-Light.woff") format("woff");
+         url("css/woff/FiraCode-Light.woff") format("woff");
     font-weight: 300;
     font-style: normal;
   }
@@ -13,7 +13,7 @@ const firaCodeStyle = `
   @font-face {
     font-family: "Fira Code";
     src: url("css/woff2/FiraCode-Regular.woff2") format("woff2"),
-    url("css/woff2/FiraCode-Regular.woff") format("woff");
+         url("css/woff/FiraCode-Regular.woff") format("woff");
     font-weight: 400;
     font-style: normal;
   }
@@ -21,19 +21,36 @@ const firaCodeStyle = `
   @font-face {
     font-family: "Fira Code";
     src: url("css/woff2/FiraCode-Medium.woff2") format("woff2"),
-    url("css/woff2/FiraCode-Medium.woff") format("woff");
+         url("css/woff/FiraCode-Medium.woff") format("woff");
     font-weight: 500;
     font-style: normal;
   }
   
   @font-face {
     font-family: "Fira Code";
+    src: url("css/woff2/FiraCode-SemiBold.woff2") format("woff2"),
+         url("css/woff/FiraCode-SemiBold.woff") format("woff");
+    font-weight: 600;
+    font-style: normal;
+  }
+  
+  @font-face {
+    font-family: "Fira Code";
     src: url("css/woff2/FiraCode-Bold.woff2") format("woff2"),
-    url("css/woff2/FiraCode-Bold.woff") format("woff");
+         url("css/woff/FiraCode-Bold.woff") format("woff");
     font-weight: 700;
     font-style: normal;
   }
-
+  
+  @font-face {
+    font-family: "Fira Code VF";
+    src: url("css/woff2/FiraCode-VF.woff2") format("woff2-variations"),
+         url("css/woff/FiraCode-VF.woff") format("woff-variations");
+    /* font-weight requires a range: https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Fonts/Variable_Fonts_Guide#Using_a_variable_font_font-face_changes */
+    font-weight: 300 700;
+    font-style: normal;
+  }
+  
 `;
 
 export default firaCodeStyle;
