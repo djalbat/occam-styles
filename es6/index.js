b17920a91058795f95ca9c237c2ceb5a26925740
James Smith <james.smith@openmathematics.org>
2020-09-05 15:41:48 +0100

    Added the monospace mixin.

--- es6/index.js
+++ es6/index.js
@@ -5,6 +5,7 @@ export { default as syntaxStyle } from "./style/syntax";
 export { default as firaCodeStyle } from "./style/firaCode";
 export { default as commonFontMixin } from "./mixin/font/common";
 export { default as firaCodeFontMixin } from "./mixin/font/firaCode";
+export { default as monospaceFontMixin } from "./mixin/font/monospace";
 export { default as clientScheme } from "./scheme/client";
 export { default as editorScheme } from "./scheme/editor";
 export { default as commonScheme } from "./scheme/common";
---
+++ es6/mixin/font/monospace.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const monospaceFontMixin = `
+
+  font-size: 1.3rem;
+  line-height: 20px;
+  font-family: monospace;
+
+`;
+
+export default monospaceFontMixin;
