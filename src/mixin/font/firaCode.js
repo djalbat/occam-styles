76f9f362032b1b419aad0d42b8b1eed59783f029
James Smith <james.smith@djalbat.com>
2022-03-15 18:30:01 +0000

    Removed the comments from the FiraCode font mixin.

--- src/mixin/font/firaCode.js
+++ src/mixin/font/firaCode.js
@@ -5,8 +5,8 @@ const firaCodeFontMixin = `
   font-size: 1.3rem;
   line-height: 20px;
   font-family: "Fira Code";
-  text-rendering: optimizeLegibility; /* Force ligatures for Webkit, Blink, Gecko */
-  font-feature-settings: "calt" 1;  /* Enable ligatures for IE 10+, Edge */
+  text-rendering: optimizeLegibility;
+  font-feature-settings: "calt" 1;
 
 `;
 
--- src/scheme/syntax/default.js
+++ src/scheme/syntax/default.js
@@ -3,4 +3,5 @@
 import { springWood, pomegranate } from "../../colours";
 
 export const colour = springWood;
+
 export const errorUnderlingColour = pomegranate;
