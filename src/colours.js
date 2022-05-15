bb94003a87b6a70fa9999f515fd925224f6db7f2
James Smith <james.smith@djalbat.com>
2022-05-15 08:36:04 +0100

    Updated the colours.

--- src/colours.js
+++ src/colours.js
@@ -1,9 +1,9 @@
 "use strict";
 
 export const springWood = "#f8f6f1";
-export const bonjour = "#e5e0e1";
-export const gainsborough = "#d5d8d2";
-export const dawn = "#a6a29a";
+export const bonjour = "#E5E2E0";
+export const gainsborough = "#D8D6D2";
+export const dawn = "#B8B3A8";
 export const stardust = "#9f9f9c";
 export const hurricane = "#908989";
 export const pablo = "#776f61";
@@ -12,20 +12,21 @@ export const emperor = "#515150";
 export const tuatara = "#363534";
 export const woodsmoke = "#0c0d0f";
 
+export const pomegranate = "#f52929";
 export const sushi = "#7fa82f";
+export const lapis = "#0030e6";
+
 export const logCabin = "#24301d";
 export const blackOlive = "#1c2412";
 export const hunterGreen = "#161d10";
 
-export const regentGrey = "#899396";
 export const cadetBlue = "#5f9ea0";
+export const regentGrey = "#899396";
 
 export const razzmatazz = "#e3256b";
 export const flamingo = "#CB2039";
 export const terracotta = "#a83a25";
 export const korma = "#99420f";
-
-export const pomegranate = "#f52929";
 export const treePoppy = "#E4902F";
 
 export const portica = "#f7e75f";
