a656b436deb67554a53de2f8ad12c7f4d45563cd
James Smith <james.smith@djalbat.com>
2022-09-03 20:33:30 +0100

    Changed all the triangles into elements.

--- src/svg/triangle/left.js
+++ src/svg/triangle/left.js
@@ -2,21 +2,28 @@
 
 import withStyle from "easy-with-style";  ///
 
+import { Element } from "easy";
+
 import { commonColour } from "../../scheme/common";
 
-const LeftTriangleSVG = (properties) => {
-  const { className } = properties;
+class LeftTriangleSVG extends Element {
+  childElements() {
+    return (
 
-  return (
-
-    <svg width="25mm" height="25mm" viewBox="0 0 25 25" className={`${className} left-triangle`}>
       <g>
         <path style="stroke-width:1" d="M 12.988592,21.371981 V 14.569568 L 6.1739528,17.970775 Z" />
       </g>
-    </svg>
 
-  );
-};
+    );
+  }
+
+  static tagName = "svg";
+
+  static defaultProperties = {
+    viewBox: "0 0 25 25",
+    className: "left-triangle"
+  };
+}
 
 export default withStyle(LeftTriangleSVG)`
 
