ec2cc79a88e76d11d4f164dbe785ae0f5aa0da6a
James Smith <james.smith@djalbat.com>
2022-11-18 11:23:25 +0000

    Added font preloading and corrected the FireCode style.

--- src/style/firaCode.js
+++ src/style/firaCode.js
@@ -7,7 +7,7 @@ const firaCodeStyle = (host = DEFAULT_HOST) => `
   @font-face {
     font-family: "Fira Code";
     src: url("${host}/css/woff2/FiraCode-Light.woff2") format("woff2"),
-    url("${host}/css/woff2/FiraCode-Light.woff") format("woff");
+    url("${host}/css/woff/FiraCode-Light.woff") format("woff");
     font-weight: 300;
     font-style: normal;
   }
@@ -15,7 +15,7 @@ const firaCodeStyle = (host = DEFAULT_HOST) => `
   @font-face {
     font-family: "Fira Code";
     src: url("${host}/css/woff2/FiraCode-Regular.woff2") format("woff2"),
-    url("${host}/css/woff2/FiraCode-Regular.woff") format("woff");
+    url("${host}/css/woff/FiraCode-Regular.woff") format("woff");
     font-weight: 400;
     font-style: normal;
   }
@@ -23,7 +23,7 @@ const firaCodeStyle = (host = DEFAULT_HOST) => `
   @font-face {
     font-family: "Fira Code";
     src: url("${host}/css/woff2/FiraCode-Medium.woff2") format("woff2"),
-    url("${host}/css/woff2/FiraCode-Medium.woff") format("woff");
+    url("${host}/css/woff/FiraCode-Medium.woff") format("woff");
     font-weight: 500;
     font-style: normal;
   }
@@ -31,7 +31,7 @@ const firaCodeStyle = (host = DEFAULT_HOST) => `
   @font-face {
     font-family: "Fira Code";
     src: url("${host}/css/woff2/FiraCode-Bold.woff2") format("woff2"),
-    url("${host}/css/woff2/FiraCode-Bold.woff") format("woff");
+    url("${host}/css/woff/FiraCode-Bold.woff") format("woff");
     font-weight: 700;
     font-style: normal;
   }
