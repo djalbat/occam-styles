b79e7a9579b5236d7944f82053191d89c3307499
James Smith <james.smith@djalbat.com>
2022-11-30 14:09:13 +0000

    Propagated.

--- src/styles.js
+++ src/styles.js
@@ -5,4 +5,4 @@ export const titleSVGHeight = "2.1rem";
 export const toggleSVGHeight = "1.8rem"
 export const markerSVGHeight = "2.1rem"
 export const markerSVGMarginLeft = "3rem";
-export const fileNameSVGPaddingLeft = "3rem";
+export const fileNameSVGPaddingLeft = "2.2rem";
