e80fbcb6a27dd00c2a3f5d929990bdd35a559462
James Smith <james.smith@djalbat.com>
2023-01-18 17:39:17 +0000

    Simplified the meta JSON styles.

--- src/scheme/syntax/metaJSON.js
+++ src/scheme/syntax/metaJSON.js
@@ -1,16 +1,7 @@
 "use strict";
 
-import { pablo, portica, hokeyPokey, terracotta } from "../../colours";
+import { hokeyPokey } from "../../colours";
 
 export const repositoryColour = hokeyPokey;
 
-export const dependencyColour = hokeyPokey;
-
 export const stringLiteralColour = hokeyPokey;
-
-export const dependencyHoverColour = portica;
-
-export const faultyDependencyColour = terracotta;
-
-export const disabledDependencyColour = pablo;
-
--- src/style/syntax/metaJSON.js
+++ src/style/syntax/metaJSON.js
@@ -1,23 +1,10 @@
 "use strict";
 
-import { repositoryColour,
-         dependencyColour,
-         stringLiteralColour,
-         dependencyHoverColour,
-         faultyDependencyColour,
-         disabledDependencyColour } from "../../scheme/syntax/metaJSON";
+import { repositoryColour, stringLiteralColour } from "../../scheme/syntax/metaJSON";
 
 const metaJSONSyntaxStyle = `
 
   .meta-json.syntax > .string-literal { color: ${stringLiteralColour}; }
-
-  .meta-json.syntax > .dependency { color: ${dependencyColour}; }
-
-  .meta-json.syntax > .dependency:hover { color: ${dependencyHoverColour}; }
-
-  .meta-json.syntax > .dependency.faulty { color: ${faultyDependencyColour}; }
-
-  .meta-json.syntax > .dependency.disabled { color: ${disabledDependencyColour}; }
 
   .meta-json.syntax > .repository, .meta-json.syntax > .repository a { color: ${repositoryColour}; }
 
