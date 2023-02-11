6c45316005c9e8657ed82a4e64edeb932afc4b79
James Smith <james.smith@djalbat.com>
2023-02-11 13:46:01 +0000

    Propagated.

--- src/colours.js
+++ src/colours.js
@@ -23,7 +23,7 @@ export const blackOlive = "#1c2412";
 export const hunterGreen = "#161d10";
 
 export const cadetBlue = "#5f9ea0";
-export const spanishGreen = "#90ae89";
+export const spanishGreen = "#90bc86";
 
 export const pomegranate = "#f52929";
 export const flamingo = "#CB2039";
