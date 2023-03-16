26afd4b9e2a8eb9a97a4abde5250fc8c4ad440e4
James Smith <james.smith@djalbat.com>
2023-03-16 11:45:54 +0000

    Improved the export custom grammars SVG considerably.

--- src/svg/toolbar/exportCustomGrammars.js
+++ src/svg/toolbar/exportCustomGrammars.js
@@ -7,8 +7,13 @@ export default class ExportCustomGrammarsToolbarSVG extends ToolbarSVG {
     return (
 
       <g>
-        <path d="m 4,4 v 17 h 13.333334 v -4.857142 h -2.666668 v 2.428571 H 6.6666666 V 6.4285714 H 14.666666 V 8.857143 h 2.666668 V 4 Z" />
+        <rect width="2" height="1" x="14" y="7" />
+        <rect width="2" height="1" x="14" y="17" />
         <path d="m 12,11.285714 h 8 V 8.8571429 l 4,3.6428581 -4,3.642856 v -2.428571 h -8 z" />
+        <rect width="12" height="2" x="4" y="5" />
+        <rect width="2" height="6.1818233" x="0.40808085" y="7.867775" transform="matrix(1,0,0.45653557,0.88970516,0,0)" />
+        <rect width="2" height="6.1818233" x="-15.236363" y="14.049598" transform="matrix(-1,0,-0.45653557,0.88970516,0,0)" />
+        <rect width="12" height="2" x="4" y="18" />
       </g>
 
     );
