b00dd7f05a8fc954875fe1d2bb8d5505fd113ea2
James Smith <james.smith@djalbat.com>
2023-03-23 19:53:49 +0000

    Propagated.

--- src/styles.js
+++ src/styles.js
@@ -1,6 +1,6 @@
 "use strict";
 
-export const foldSVGWidth = "1.2rem"
+export const foldSVGWidth = "1rem"
 export const logoSVGWidth = "24rem"
 export const nameSVGHeight = "2.1rem"
 export const titleSVGHeight = "2.1rem";
