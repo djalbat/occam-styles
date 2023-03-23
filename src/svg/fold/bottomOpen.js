5a8397a187bb97cab325c6d8631ef8ad81d5388e
James Smith <james.smith@djalbat.com>
2023-03-23 19:37:36 +0000

    Propagated.

--- src/svg/fold/bottomOpen.js
+++ src/svg/fold/bottomOpen.js
@@ -7,7 +7,7 @@ export default class BottomOpenFoldSVG extends Fold {
     return (
 
       <g>
-        <path d="M 7.9999971,2.8436987 2.04215,8.6171677 c 0,0 0,5.7734703 0,5.7734703 H 13.957849 V 8.6171677 Z" style="stroke-width:4;paint-order:markers stroke fill; stroke-linejoin: round;" />
+        <path d="M 7.9999971,2.8436987 2.04215,8.6171677 c 0,0 0,5.7734703 0,5.7734703 H 13.957849 V 8.6171677 Z" style="stroke-width:4;paint-order:markers stroke fill;stroke-linejoin: round;" />
         <path d="m 4,11 h 8" style="stroke-width:3" />
       </g>
 
