d6dc95a968294c12154e037472a60d5fdcb99420
James Smith <james.smith@djalbat.com>
2023-04-12 19:38:22 +0100

    Propagated.

--- src/scheme/syntax/default.js
+++ src/scheme/syntax/default.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import { tuatara, springWood, pomegranate, gainsborough } from "../../colours";
+import { dune, springWood, pomegranate, gainsborough } from "../../colours";
 
 export const colour = springWood;
 
@@ -10,4 +10,4 @@ export const nonsenseColour = gainsborough;
 
 export const underlineColour = pomegranate;
 
-export const backgroundColour = tuatara;
+export const backgroundColour = dune;
