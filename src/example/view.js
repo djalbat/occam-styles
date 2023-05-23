bd7e1fbc9430a24fda40bf640468d0e41a530282
James Smith <james.smith@djalbat.com>
2023-05-23 10:10:27 +0100

    Corrected the example.

--- src/example/view.js
+++ src/example/view.js
@@ -2,15 +2,13 @@
 
 import { Element } from "easy";
 
-import ReleaseNameSVG from "../svg/name/release";
 import DirectoryNameSVG from "../svg/name/directory";
 
 export default class View extends Element {
   childElements() {
     return ([
 
-      <ReleaseNameSVG/>,
-      <DirectoryNameSVG/>,
+      <DirectoryNameSVG/>
 
     ]);
   }
