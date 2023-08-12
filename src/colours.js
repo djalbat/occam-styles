fb217c88618d91c3fc4994cb66395e093b96250b
James Smith <james.smith@djalbat.com>
2023-08-12 15:30:36 +0100

    Some improvedments to the colours. Added precedence to the custom grammar BNF syntax style.

--- src/colours.js
+++ src/colours.js
@@ -1,34 +1,37 @@
 "use strict";
 
-export const springWood = "#fffdfc";
+export const springWood = "#f8f6f1";
 export const bianca = "#f8f7eb";
-export const bonjour = "#f1efee";
+export const bonjour = "#E5E2E0";
 export const gainsborough = "#D8D6D2";
 export const dawn = "#B8B3A8";
 export const stardust = "#9f9f9c";
 export const hurricane = "#908989";
-export const pablo = "#68655f";
+export const pablo = "#776f61";
 export const stormDust = "#646463";
 export const emperor = "#515150";
 export const tuatara = "#363534";
 export const dune = "#24211e";
 export const woodsmoke = "#0c0d0f";
 
-export const razzmatazz = "#e3256b";
+export const pomegranate = "#f52929";
 export const sushi = "#7fa82f";
+export const lapis = "#0030e6";
 
 export const logCabin = "#24301d";
 export const blackOlive = "#1c2412";
 export const hunterGreen = "#161d10";
 
 export const cadetBlue = "#5f9ea0";
+export const regentGrey = "#899396";
+
 export const spanishGreen = "#84b07b";
 
-export const pomegranate = "#f52929";
+export const razzmatazz = "#e3256b";
+export const terracotta = "#a83a25";
 export const korma = "#99420f";
 export const walnut = "#59331c";
 
-export const portica = "#f7e75f";
 export const hokeyPokey = "#c6b624";
 export const celery = "#bcb852";
 export const sycamore = "#908d39";
@@ -50,15 +53,17 @@ export default {
   woodsmoke,
   pomegranate,
   sushi,
+  lapis,
   logCabin,
   blackOlive,
   hunterGreen,
   cadetBlue,
+  regentGrey,
   spanishGreen,
+  razzmatazz,
+  terracotta,
   korma,
   walnut,
-  razzmatazz,
-  portica,
   hokeyPokey,
   celery,
   sycamore,
--- src/scheme/syntax/customGrammarBNF.js
+++ src/scheme/syntax/customGrammarBNF.js
@@ -1,9 +1,11 @@
 "use strict";
 
-import { cadetBlue, hokeyPokey, spanishGreen } from "../../colours";
+import { cadetBlue, hokeyPokey, regentGrey, spanishGreen } from "../../colours";
 
 export const nameColour = cadetBlue;
 
 export const typeColour = spanishGreen;
+
+export const precedenceColour = regentGrey;
 
 export const stringLiteralColour = hokeyPokey;
