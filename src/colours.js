e4ab5e595f66268a53c6531e5fa8b5198ac32de1
James Smith <james.smith@djalbat.com>
2023-10-03 15:58:44 +0100

    Propagated.

--- src/colours.js
+++ src/colours.js
@@ -32,7 +32,7 @@ export const terracotta = "#a83a25";
 export const korma = "#99420f";
 export const walnut = "#59331c";
 
-export const sunflower = "#c6b624";
+export const sunflower = "#c9bf62";
 export const hokeyPokey = "#c6b624";
 export const celery = "#bcb852";
 export const sycamore = "#908d39";
