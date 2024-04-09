24d578eb7116e0416db55bab36cfdfb79a2aa2b7
James Smith <james.smith@djalbat.com>
2024-04-09 19:05:41 +0100

    Propagated.

--- src/style/juliaMono.js
+++ src/style/juliaMono.js
@@ -5,7 +5,7 @@ import { DEFAULT_HOST } from "../defaults";
 const juliaMonoStyle = (host = DEFAULT_HOST) => `
 
   @font-face {
-    src: url("${host}/css/woff2/JuliaMono-Regular.woff2");
+    src: url("${host}font/JuliaMono-Regular.woff2");
     font-family: "JuliaMono";
     font-weight: normal;
   }
