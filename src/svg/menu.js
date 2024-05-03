d45c2f7dd828f6f56e093091769c1d737526f9a9
James Smith <james.smith@djalbat.com>
2024-05-03 15:58:42 +0100

    Another cosmetic correction.

--- src/svg/menu.js
+++ src/svg/menu.js
@@ -2,7 +2,7 @@
 
 import SVG from "../svg";
 
-export default class FontSizeSVG extends SVG {
+export default class MenuSVG extends SVG {
   static defaultProperties = {
     viewBox: "0 0 25 25",
     className: "font-size"
