b6b0b6dc833c2e3b20db136ea3c6b6b7e43459a7
James Smith <james.smith@djalbat.com>
2024-06-04 13:16:56 +0100

    Further work on the Markdown syntax style.

--- src/scheme/syntax/markdown.js
+++ src/scheme/syntax/markdown.js
@@ -1,15 +1,21 @@
 "use strict";
 
-import { toast, stardust, sycamore, cadetBlue, spanishGreen, gainsborough } from "../../colours";
+import { toast, celery, stardust, sycamore, cadetBlue, hokeyPokey, spanishGreen, gainsborough } from "../../colours";
 
 export const colour = gainsborough;
 
+export const pathColour = celery;
+
 export const tableColour = sycamore;
+
+export const markerColour = toast;
+
+export const bulletColour = toast;
 
 export const headingColour = spanishGreen;
 
 export const listingColour = stardust;
 
-export const listItemColour = toast;
-
 export const directiveColour = cadetBlue;
+
+export const pathHoverColour = hokeyPokey;
--- src/style/syntax/markdown.js
+++ src/style/syntax/markdown.js
@@ -1,19 +1,25 @@
 "use strict";
 
 import { errorColour, errorTextDecorationColour } from "../../scheme/syntax/default";
-import { colour, tableColour, listingColour, headingColour, listItemColour, directiveColour } from "../../scheme/syntax/markdown";
+import { colour, pathColour, tableColour, markerColour, bulletColour, listingColour, headingColour, directiveColour, pathHoverColour } from "../../scheme/syntax/markdown";
 
 const markdownSyntaxStyle = `
 
   .markdown.syntax > div > * { color: ${colour}; }
 
+  .markdown.syntax > div > .path { color: ${pathColour}; }
+
+  .markdown.syntax > div > .path:hover { color: ${pathHoverColour}; }
+
   .markdown.syntax > div > .table { color: ${tableColour}; }
+
+  .markdown.syntax > div > .marker { color: ${markerColour}; }
+
+  .markdown.syntax > div > .bullet { color: ${bulletColour}; }
 
   .markdown.syntax > div > .listing { color: ${listingColour}; }
 
   .markdown.syntax > div > .heading { color: ${headingColour}; }
-
-  .markdown.syntax > div > .list-item { color: ${listItemColour}; }
 
   .markdown.syntax > div > .directive { color: ${directiveColour}; }
 
