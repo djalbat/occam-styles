16e3a0b8db9900556539fb1d66c8d387d1a110c9
James Smith <james.smith@djalbat.com>
2024-06-28 18:55:24 +0100

    Fixed the JSON class selectors.

--- src/style/syntax/json.js
+++ src/style/syntax/json.js
@@ -5,11 +5,11 @@ import { errorColour, stringLiteralColour, errorTextDecorationColour } from "../
 
 const jsonSyntaxStyle = `
 
-  .json.syntax > div > .number { color: ${numberColour}; }
+  [class$="-json"].syntax > div > .number { color: ${numberColour}; }
 
-  .json.syntax > div > .string-literal { color: ${stringLiteralColour}; }
+  [class$="-json"].syntax > div > .string-literal { color: ${stringLiteralColour}; }
 
-  .json.syntax > div > .error {
+  [class$="-json"].syntax > div > .error {
     color: ${errorColour};
     text-decoration-line: underline;
     text-decoration-color: ${errorTextDecorationColour};
