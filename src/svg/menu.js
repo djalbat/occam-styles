174397b295c36656c358d40f66f134aed88126a9
James Smith <james.smith@djalbat.com>
2025-12-04 20:22:34 +0000

    Added an explicit width and height to the menu SVGs.

--- src/svg/menu.js
+++ src/svg/menu.js
@@ -1,10 +1,19 @@
 "use strict";
 
+import withStyle from "easy-with-style";  ///
+
 import SVG from "../svg";
 
-export default class MenuSVG extends SVG {
+class MenuSVG extends SVG {
   static defaultProperties = {
     viewBox: "0 0 25 25",
     className: "font-size"
   };
 }
+
+export default withStyle(MenuSVG)`
+
+  width: 100%;
+  height: 100%;
+  
+`;
